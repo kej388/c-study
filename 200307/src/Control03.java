9cbcfa92ca1c08f804b8f1740b05577bb35ff8dd
kej388 <eunjinkoh96@gmail.com>
2020-03-07 13:28:33 +0900

    반복문 연습

--- 200307/src/Control03.java
+++ 200307/src/Control03.java
@@ -1,28 +1,31 @@
-package control;
-
 import java.util.Scanner;
 
-public class control04 {
+public class Control03 {
 
 	public static void main(String[] args) {
 		// 자연수 n을 입력받아서 다음과 같이 출력하는 프로그램을 작성하시오.
+		// 
 		/*
-		  출력예)
-		  -------------
-			 *****
-			  ***
-			   * 
-		  -------------
+		 * 출력 예 )
+		 * ---------------
+		 		***
+		 		 **
+		 		  *
+		  ----------------
 		 * */
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for(int i = 0; i < n ; i++) {
+		
+		for(int i = 0; i < n; i++) {
+			
 			for(int j = n; j > i; j--) {
 				System.out.print("*");
-				
 			}
 			System.out.println();
-			System.out.print(" ");
+			for(int k = 0; k < i + 1; k++) {
+				
+				System.out.print(" ");				
+			}
 		}
 
 	}
--- 200307/src/Control04.java
+++ 200307/src/Control04.java
@@ -1,33 +1,26 @@
-package control;
-
 import java.util.Scanner;
 
-public class control03 {
+public class Control04 {
 
 	public static void main(String[] args) {
 		// 자연수 n을 입력받아서 다음과 같이 출력하는 프로그램을 작성하시오.
-		// 
 		/*
-		 * 출력 예 )
-		 * ---------------
-		 		***
-		 		 **
-		 		  *
-		  ----------------
+		  출력예)
+		  -------------
+			 *****
+			  ***
+			   * 
+		  -------------
 		 * */
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			
+		for(int i = 0; i < n ; i++) {
 			for(int j = n; j > i; j--) {
 				System.out.print("*");
+				
 			}
 			System.out.println();
-			for(int k = 0; k < i + 1; k++) {
-				
-				System.out.print(" ");				
-			}
+			System.out.print(" ");
 		}
 
 	}
